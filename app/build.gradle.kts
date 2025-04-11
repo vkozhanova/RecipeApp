491909b818053de56be5546355a970ae253ee497
vkozhanova <kozhanova_vera@mail.ru>
2025-04-11 22:50:16 +0500

    RA-02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myapplication"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
