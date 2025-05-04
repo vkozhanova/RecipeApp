41b2bb602d3d9b1b84c95b5d4f54b58d60379ae8
vkozhanova <kozhanova_vera@mail.ru>
2025-05-04 19:19:46 +0500

    RA-05-basic-button-navigation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,26 +1,46 @@
 package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.example.myapplication.fragments.CategoriesListFragment
+import com.example.myapplication.fragments.FavoritesFragment
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding
         get() = _binding!!
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                replace<CategoriesListFragment>(R.id.mainContainer)
-            }
+        if (savedInstanceState == null) {
+            showFragment(CategoriesListFragment())
+        }
+        setupNavigation()
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.mainContainer, fragment)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+
+    }
+
+    private fun setupNavigation() {
+        binding.btnCategories.setOnClickListener {
+           showFragment(CategoriesListFragment())
+
+        }
+        binding.btnFavorites.setOnClickListener {
+            showFragment(FavoritesFragment())
+
         }
     }
 
