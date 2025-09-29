823f63c26b07836b1048329b00abc6a8fe1fa6f0
vkozhanova <kozhanova_vera@mail.ru>
2025-09-30 01:17:31 +0500

    RA-38

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
